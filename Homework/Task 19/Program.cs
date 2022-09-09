d12620be036b259f45df38b569efc52401782208
lubovcha <chalovaljubov@mail.ru>
2022-09-10 02:54:09 +0300

    1

--- Homework/Task 19/Program.cs
+++ Homework/Task 19/Program.cs
@@ -9,3 +9,12 @@
 23432 -> да
 */
 
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine()); //задаем переменную anyNumber
+string anyNumberText = Convert.ToString(anyNumber); // переводим в ряд чисел
+
+if (anyNumberText[0] == anyNumberText[4] && anyNumberText[1] == anyNumberText[3]) //тут не ставятся {} внутри совместно с []}
+    Console.WriteLine("Число является палиндромом"); 
+else{
+    Console.WriteLine("Число не является палиндромом");
+}
